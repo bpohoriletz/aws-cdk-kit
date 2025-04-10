f2bb5c186f4d2d31d3f67a0197c6c7e3daf4aeb4
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-04-10 11:04:07 +0300

    [TES] added codedeploy app test

---
+++ codedeploy/app.test.ts
@@ -0,0 +1,24 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+
+import * as app from "./app";
+
+describe("CodeDeploy .createCdApplication()", () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test('matches snapshot', () => {
+    app.createCdApplication(["test"], stack);
+
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+
+  test('has proper name', () => {
+    app.createCdApplication(["name"], stack);
+
+    Template.fromStack(stack).hasResourceProperties("AWS::CodeDeploy::Application", { "ApplicationName": "name-cd-app" })
+  });
+});
