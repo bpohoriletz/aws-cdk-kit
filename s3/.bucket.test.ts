aefd4191bfc19392ddf6cf5f342812c431680394
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-04-12 11:52:20 +0300

    [TES] Added tests

---
+++ s3/.bucket.test.ts
@@ -0,0 +1,32 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+
+import * as func from "./bucket";
+
+describe("S3 .createPrivateBucket()", () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test("matches snapshot", () => {
+    func.createPrivateBucket(["pre", "fix"], stack);
+
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+});
+
+describe("S3 .createEbBucket()", () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test("matches snapshot", () => {
+    func.createEbBucket("prefix", stack);
+
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+});
