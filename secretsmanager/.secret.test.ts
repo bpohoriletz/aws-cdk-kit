e78895249ab7e763574937567ef9a1eae987354e
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-04-14 17:21:55 +0300

    [TES] Added tests

---
+++ secretsmanager/.secret.test.ts
@@ -0,0 +1,33 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { stub } from "../test/stubs"
+
+import * as func from "./secret";
+
+describe("SecretsManager .createSecret()", () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test("matches snapshot", () => {
+    func.createSecret(["pre", "fix"], stack, "details")
+
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+});
+
+describe("SecretsManager .createProdSecret()", () => {
+  let stack: cdk.Stack;
+
+  beforeEach(() => {
+    stack = new cdk.Stack();
+  });
+
+  test("matches snapshot", () => {
+    func.createProdSecret(["pre", "fix"], stack, "details")
+
+    expect(Template.fromStack(stack)).toMatchSnapshot();
+  });
+});
