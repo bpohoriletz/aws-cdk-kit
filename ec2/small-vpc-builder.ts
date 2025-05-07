ba65deece8e17208742ad5fbcb13c864e724bab1
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-07 22:08:27 +0300

    [REF] Added builders for regular and production VPC

--- ec2/small-vpc-builder.ts
+++ ec2/small-vpc-builder.ts
@@ -1,50 +1,9 @@
-import * as ec2 from "aws-cdk-lib/aws-ec2";
-import { IVpcBuilder, VpcProduct } from "../products/vpc";
-import * as conf from "../../config/ec2/vpc";
+import VpcBuilder from "./vpc-builder"
 
-export default class SmallVpcBuilder implements IVpcBuilder {
-  private resourceName: string;
-  private vpcProps: VpcProduct;
-
-  constructor(resourceName: string) {
-    this.resourceName = resourceName;
-    this.vpcProps = new VpcProduct();
-  }
-
-  customizeCidr(): this {
-    this.vpcProps.setCidr(conf.vpcCidrConfig(this.resourceName));
-
-    return this;
-  }
+export default class SmallVpcBuilder extends VpcBuilder {
   customizeAzs(): this {
     this.vpcProps.setMaxAzs(1);
 
     return this;
   }
-  customizeNatProvider(): this {
-    const provider = ec2.NatProvider.instanceV2({
-      instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
-        ec2.InstanceSize.NANO
-      ),
-    });
-    this.vpcProps.setNatProvider(provider);
-
-    return this;
-  }
-  customizeNatSubnets(): this {
-    this.vpcProps.setNatSubnets({ subnetType: ec2.SubnetType.PUBLIC });
-
-    return this;
-  }
-  customizeSubnetConfiguration(): this {
-    const config = conf.vpcSubnetConfig(this.resourceName);
-    this.vpcProps.setSubnetConfiguration(config)
-
-    return this;
-  }
-
-  getResult(): VpcProduct {
-    return this.vpcProps;
-  };
 }
