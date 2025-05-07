b942440be11a36976988dc18e2de1e160c5ff225
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-07 22:50:56 +0300

    [TES] Updated snapshots

--- directors/.vpc.test.ts
+++ directors/.vpc.test.ts
@@ -1,11 +1,11 @@
 import VpcDirector from "./vpc"
-import SmallVpcBuilder from "../ec2/small-vpc-builder"
+import PetVpcBuilder from "../ec2/vpc-builders/pet"
 
 describe("new VpcDirector()", () => {
   let instance: VpcDirector;
 
   beforeEach(() => {
-    instance = new VpcDirector(["stack"], SmallVpcBuilder);
+    instance = new VpcDirector("Vpc", PetVpcBuilder);
   });
 
   test("construct vpc for pet projects", () => {
