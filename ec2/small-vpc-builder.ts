5dbb71b3064059075e866ee54e283a62b75b96ef
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-07 22:40:40 +0300

    [REF] Moved builders to a separate folder

--- ec2/small-vpc-builder.ts
+++
@@ -1,9 +0,0 @@
-import VpcBuilder from "./vpc-builder"
-
-export default class SmallVpcBuilder extends VpcBuilder {
-  customizeAzs(): this {
-    this.vpcProps.setMaxAzs(1);
-
-    return this;
-  }
-}
