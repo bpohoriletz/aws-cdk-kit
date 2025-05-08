9edeea00d11924828a5fc2b00cccf498c3553d4b
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-08 17:47:55 +0300

    [REF] Added Director/Builder/Product for SecurityGroup

---
+++ products/.security-group.test.ts
@@ -0,0 +1,13 @@
+import { SecurityGroupProduct } from "./security-group"
+
+describe("new SecurityGroupProduct()", () => {
+  let instance: SecurityGroupProduct;
+
+  beforeEach(() => {
+    instance = new SecurityGroupProduct();
+  });
+
+  test("has correct defaults", () => {
+    expect(instance).toMatchSnapshot();
+  });
+});
