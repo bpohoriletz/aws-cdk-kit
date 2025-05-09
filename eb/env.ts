804a0b4dbdebde085f021f66b51edc1efff4be56
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-09 09:25:49 +0300

    [REF] Used SgDirector/Builder instead of functions

--- eb/env.ts
+++ eb/env.ts
@@ -3,6 +3,8 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { CfnApplication, CfnEnvironment } from "aws-cdk-lib/aws-elasticbeanstalk";
 import * as iam from "aws-cdk-lib/aws-iam";
 
+import SecurityGroupDirector from "../directors/security-group";
+import SecurityGroupBuilder from "../ec2/security-group-builder";
 import { envOptionSettings } from "./setting";
 import * as con from "../utils/naming";
 
@@ -14,7 +16,7 @@ export async function createEnvironment(
   stack: Stack,
   solutionStackName: string) : Promise<[CfnEnvironment, ec2.SecurityGroup]> {
     const environmentName = con.ebEnvironmentName(resourceNamePrefix);
-    const securityGroups = createSecurityGroups(resourceNamePrefix, vpc, stack);
+    const securityGroups = createSecurityGroups(stack, vpc);
 
     const env = new CfnEnvironment(stack, environmentName, {
       applicationName: application.applicationName!,
@@ -35,37 +37,10 @@ export async function createEnvironment(
   }
 
 // Private
-function createSecurityGroups(resourceNamePrefix: string[], vpc: ec2.IVpc, stack: Stack): ec2.SecurityGroup[] {
-  // Create Security Group for load balancer
-  const resourceName = con.ec2SecurityGroupName(resourceNamePrefix.slice(0,3), "lb");
-  const lbSecurityGroup = new ec2.SecurityGroup(stack, resourceName , {
-    vpc: vpc,
-    description: "Security Group for the Load Balancer",
-    securityGroupName: resourceName,
-    allowAllOutbound: true
-  });
-
-  // Allow Security Group inbound traffic for load balancer
-  lbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow incoming traffic over port 80");
-  lbSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443), "Allow incoming traffic over port 443");
-
-  // Create Security Group for web instances
-  const webSecurityGroup = createWebSg(resourceNamePrefix, vpc, stack);
-  // Allow Security Group inbound traffic over port 80 from the Load Balancer security group
-  webSecurityGroup.connections.allowFrom( new ec2.Connections({securityGroups: [lbSecurityGroup]}), ec2.Port.tcp(80));
+function createSecurityGroups(stack: Stack, vpc: ec2.IVpc): ec2.SecurityGroup[] {
+  const lbSecurityGroup = new SecurityGroupDirector(SecurityGroupBuilder).constructWebSecurityGroup(stack, "LbSecurityGroup", vpc);
+  const webSecurityGroup = new SecurityGroupDirector(SecurityGroupBuilder).constructWebSecurityGroup(stack, "WebSecurityGroup", vpc);
+  webSecurityGroup.connections.allowFrom(new ec2.Connections({ securityGroups: [lbSecurityGroup] }), ec2.Port.tcp(80));
 
   return [lbSecurityGroup, webSecurityGroup];
-}
-
-function createWebSg(resourceNamePrefix: string[], vpc: ec2.IVpc, stack: Stack): ec2.SecurityGroup {
-  // Create Security Group for web instances
-  const resourceName = con.ec2SecurityGroupName(resourceNamePrefix.slice(0,3), "web");
-  const webSecurityGroup = new ec2.SecurityGroup(stack, resourceName, {
-    vpc: vpc,
-    description: "Security Group for the Web instances",
-    securityGroupName: resourceName,
-    allowAllOutbound: true
-  });
-
-  return webSecurityGroup;
 }
