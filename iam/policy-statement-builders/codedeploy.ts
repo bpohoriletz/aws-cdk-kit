df67fc8788bceeacd4eb2182c0e323aba5239196
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-10 15:10:40 +0300

    [REF] Added D/B/P for specific Policies

---
+++ iam/policy-statement-builders/codedeploy.ts
@@ -0,0 +1,26 @@
+import { IPolicyStatementBuilder, PolicyStatementProduct } from '../../products/policy-statement';
+
+export default class CodedeployPolicyStatementBuilder implements IPolicyStatementBuilder {
+  private policyStatementProps: PolicyStatementProduct;
+
+  constructor() {
+    this.policyStatementProps = new PolicyStatementProduct();
+  }
+
+  setFullAccess(): this {
+   this.policyStatementProps
+     .addAction('codedeploy:*');
+
+     return this;
+  }
+
+  addResource(resource: string): this {
+    this.policyStatementProps.addResource(resource);
+
+    return this;
+  }
+
+  getResult(): PolicyStatementProduct {
+    return this.policyStatementProps;
+  }
+}
--- roles/ec2.ts
+++ roles/ec2.ts
@@ -1,7 +1,9 @@
-import { Role, RoleProps, ServicePrincipal, ManagedPolicy, Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Role, RoleProps, ServicePrincipal, ManagedPolicy, Policy } from 'aws-cdk-lib/aws-iam';
 import { Stack } from 'aws-cdk-lib';
 import PolicyStatementDirector from '../directors/policy-statement';
 import S3PolicyStatementBuilder from '../iam/policy-statement-builders/s3';
+import LogsPolicyStatementBuilder from '../iam/policy-statement-builders/logs';
+import ElasticBPolicyStatementBuilder from '../iam/policy-statement-builders/elasticb';
 import * as con from '../utils/naming';
 
 export function createEc2Role(resourceNamePrefix: string[], bucketArns: string[], stack: Stack): Role {
@@ -13,31 +15,17 @@ export function createEc2Role(resourceNamePrefix: string[], bucketArns: string[]
   };
   const ec2Role = new Role(stack, con.iamInstanceRoleName(resourceNamePrefix), roleProps);
   const resources = bucketArns.flatMap((arn) => [arn, `${arn}/resources/*`]);
-  const allowEc2UploadLogsToEbRegionalBucket: Policy = new Policy(stack, 'allowEc2UploadLogsToEbRegionalBucket', {
+  new Policy(stack, 'allowEc2UploadLogsToEbRegionalBucket', {
     statements: [new PolicyStatementDirector(S3PolicyStatementBuilder).constructS3ReadWritePolicyStatement(resources)],
-  });
+  }).attachToRole(ec2Role);
 
-  allowEc2UploadLogsToEbRegionalBucket.attachToRole(ec2Role);
+  new Policy(stack, 'allowEc2PutStatistics', {
+    statements: [new PolicyStatementDirector(ElasticBPolicyStatementBuilder).constructEbEC2StatsPolicyStatement()],
+  }).attachToRole(ec2Role);
 
-  const allowEc2PutStatistics: Policy = new Policy(stack, 'allowEc2PutStatistics', {
-    statements: [
-      new PolicyStatement({
-        actions: ['elasticbeanstalk:PutInstanceStatistics'],
-        resources: ['*'],
-      }),
-    ],
-  });
-  allowEc2PutStatistics.attachToRole(ec2Role);
-
-  const allowEc2UploadLogs: Policy = new Policy(stack, 'allowEc2UploadLogs', {
-    statements: [
-      new PolicyStatement({
-        actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
-        resources: ['*'],
-      }),
-    ],
-  });
-  allowEc2UploadLogs.attachToRole(ec2Role);
+  new Policy(stack, 'allowEc2UploadLogs', {
+    statements: [new PolicyStatementDirector(LogsPolicyStatementBuilder).constructLogsUploadPolicyStatement()],
+  }).attachToRole(ec2Role);
 
   return ec2Role;
 }
