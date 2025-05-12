535dbcf000ca3cbc37e7d2f5e1e86bce0e1c1476
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-05-12 14:38:59 +0300

    [FIX] Removed sudo

--- scripts/haproxy/03_http_https_fw.sh
+++ scripts/haproxy/03_http_https_fw.sh
@@ -1,6 +1,6 @@
-sudo iptables -L INPUT -n --line-numbers
-sudo iptables -D INPUT 5
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-sudo iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
-sudo service iptables save
+iptables -L INPUT -n --line-numbers &&
+  iptables -D INPUT 5 &&
+  iptables -A INPUT -p tcp --dport 80 -j ACCEPT &&
+  iptables -A INPUT -p tcp --dport 443 -j ACCEPT &&
+  iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited &&
+  service iptables save
