2440a2b2fce8c15440a7da3de300ec9c1bc988af
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-06-24 17:47:18 +0300

    [FEA] Added HAProxy API

---
+++ scripts/haproxy/04_haproxy_api.sh
@@ -0,0 +1,3 @@
+curl -LO https://github.com/haproxytech/dataplaneapi/releases/latest/download/dataplaneapi_3.2.1_linux_amd64.rpm
+sudo dnf install -y ./dataplaneapi_3.2.1_linux_amd64.rpm
+/usr/sbin/dataplaneapi
