1220ae1e0cc0c7d4189e4172ac5c1a21fe131c6e
Bohdan Pohorilets <bohdan.pohorilets@gmail.com>
2025-06-24 17:51:30 +0300

    [FIX] Added checks to API script

--- scripts/haproxy/04_haproxy_api.sh
+++ scripts/haproxy/04_haproxy_api.sh
@@ -1,3 +1,5 @@
 curl -LO https://github.com/haproxytech/dataplaneapi/releases/latest/download/dataplaneapi_3.2.1_linux_amd64.rpm
 sudo dnf install -y ./dataplaneapi_3.2.1_linux_amd64.rpm
-/usr/sbin/dataplaneapi
+
+sudo systemctl start dataplaneapi
+curl -X GET --user admin:adminpwd http://localhost:5555/v3/info
